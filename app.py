98cf00a17b584bc0e49884e27b53825841cf60c4
Mickey248 <mickeynaja246@gmail.com>
2019-02-04 12:30:36 +0700

    make it better

--- app.py
+++ app.py
@@ -1,115 +1,130 @@
-from flask import Flask, request, abort
+import requests
+from linebot.models import *
+from linebot.models.template import *
+import json
 from linebot import (
     LineBotApi, WebhookHandler
 )
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-import requests
-#     MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselTemplate
-# )
-# import json
-# from bson.json_util import dumps
-
-# from flask.logging import create_logger
-# from flask import current_app as app
-app = Flask(__name__)
-# api = Api(app)
-
-
+# line_bot_api = LineBotApi('8Q/36jEbtoR+8iEpTPd6t3wUzAwQQ5SXe6u8jRXLTAGBpreJuoBdSWguLxyPpHJ3EteJFaoU7Au1CpQbASQd7P7Vhoe94caQotwVkKg+xxQudC7NeO+plQx0Qch0MrKnFZyDwwKmff0J8SUU9XNeegdB04t89/1O/w1cDnyilFU=')
 line_bot_api = LineBotApi('2P+U0QYQ8O7OlwBMEV1gzHRtNjLtRtEPeqj2wrhNmWpqbUdUxh6ArF0PzzFfBLHpIhbed+10m7dDzSdAOUZGJJEmEYXMu4NyWK5sFd30pu7TL7HSJ46iLMqOKEh7Dce/FEEyh3KsDs3/mYMGkVSq0AdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('7641cb5f9a109c3159ac2ce3505aa59c')
 
+def generate_news_flex(nimg,nheader,ncontent):
+  nflex = '''{
+    "type": "bubble",
+    "header": {
+      "type": "box",
+      "layout": "horizontal",
+      "contents": [
+        {
+          "type": "text",
+          "wrap":false,
+          "text": "ข่าวกีฬา",
+          "weight": "bold",
+          "color": "#aaaaaa",
+          "size": "sm"
+        }
+      ]
+    },
+    "hero": {
+      "type": "image",
+      "url": "%s",
+      "size": "full",
+      "aspectRatio": "20:13",
+      "aspectMode": "cover",
+      "action": {
+        "type": "uri",
+        "uri": "http://linecorp.com/"
+      }
+    },
+    "body": {
+      "type": "box",
+      "layout": "horizontal",
+      "spacing": "xl",
+      "flex":4,
+      "contents": [
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "lg",
+          "contents": [
+            {
+              "type": "text",
+              "wrap": true,
+              "text": "%s",
+              "size": "xs"
+            },
+            {
+              "type": "filler"
+            },
+            {
+              "type": "text",
+              "wrap": true,
+              "text": "%s",
+              "size": "xs"
+            }
+          ]
+        }
+      ]
+    }
+  }'''%(nimg,nheader,ncontent)
+  nflex = nflex.strip()
+  return nflex
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+'''
+"footer": {
+  "type": "box",
+  "layout": "horizontal",
+  "contents": [
+    {
+      "type": "button",
+      "action": {
+        "type": "uri",
+        "label": "More",
+        "uri": "https://linecorp.com"
+      }
+    }
+  ]
+}
+'''
+# a = generate_news_flex(nimg,nheader,ncontent)
+# print(nimg)
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+def create_news_flex():
+  url = "https://news-botnoi-api.herokuapp.com/news?category=sports"
+  res = requests.get(url).json()
+  res = res[::-1][0:10]
+  
+  bubbleList = ''
+  for i in range(len(res)):
+    print(i)
+    firstnews = res[i]
+    ncontent = firstnews['Detail'].replace('"','')
+    nheader = firstnews['Header'].replace('"','')
+    nimage = firstnews['photolink'].replace('"','')
+    print(nimage)
+    if nimage == 'nonePhoto':
+      nimage = 'https://www.zambianobserver.com/wp-content/uploads/2016/08/sportnews-e1470828995760.jpg'
+    flexmsg = generate_news_flex(nimage,nheader,ncontent)
+    #return flexmsg
+    bubbleList = bubbleList+flexmsg+','
+  bubbleList = bubbleList[0:-1]
+  flexmsg = '''{
+    "type": "carousel",
+    "contents":[%s]
+    }
+  '''%bubbleList
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    ball = requests.get('https://scrape-score-ball.herokuapp.com/').json()
-    print(ball)
-    carousel_template_message = TemplateSendMessage(
-    alt_text='Carousel template',
-    template=CarouselTemplate(
-        columns=[
-            CarouselColumn(
-                thumbnail_image_url='https://example.com/item1.jpg',
-                title='this is menu1',
-                text='description1',
-                actions=[
-                    PostbackAction(
-                        label='postback1',
-                        text='postback text1',
-                        data='action=buy&itemid=1'
-                    ),
-                    MessageAction(
-                        label='message1',
-                        text='message text1'
-                    ),
-                    URIAction(
-                        label='uri1',
-                        uri='http://example.com/1'
-                    )
-                ]
-            ),
-            CarouselColumn(
-                thumbnail_image_url='https://example.com/item2.jpg',
-                title='this is menu2',
-                text='description2',
-                actions=[
-                    PostbackAction(
-                        label='postback2',
-                        text='postback text2',
-                        data='action=buy&itemid=2'
-                    ),
-                    MessageAction(
-                        label='message2',
-                        text='message text2'
-                    ),
-                    URIAction(
-                        label='uri2',
-                        uri='http://example.com/2'
-                    )
-                ]
-            )
-        ]
-    )
-)
-    if 'test' in event.message.text:
-        line_bot_api.reply_message(event.reply_token, carousel_template_message)
-    else:
-    # print(event.message.text)
-    # print(type(event.message.text))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+  flexmsg = json.loads(flexmsg)
+  flexobj = FlexSendMessage('alttext',flexmsg)
+  return flexobj
+a = create_news_flex()
+print(a)
+  #userId = 'Ue8dfc92bf6d5249f02c82147e2432ffb'
+  #line_bot_api.push_message(userId,flexobj)
+  #return flexobj
+  #return ncontent,nheader,nimage
 
 
-# class test(Resource):
-#     def get(self):
-#         try:
-#             query = {'test'}
-#             listData = []
-#             for element in query:
-#                 listData.append(element)
-#             return jsonify(listData)
-#         except:
-#             return 'Not found'
 
-# api.add_resource(test, '/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
